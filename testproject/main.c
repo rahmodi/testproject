83451fec1af0ada08f635ba9cba856d192d736dc
Rahul Modi <rahulmodi@Rahuls-MacBook-Air.local>
2011-08-05 21:49:10 -0400

    Initial Commit

---
+++ testproject/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  testproject
+//
+//  Created by Rahul Modi on 8/5/11.
+//  Copyright 2011 University of Michigan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
